5e903789a3a3533bf70359326ce08cce16d400e5
David Lahai <128908238+Bombaligithud@users.noreply.github.com>
2023-03-26 00:21:23 +0530

    Add files via upload

---
+++ David.js
@@ -0,0 +1,6 @@
+var typed = new Typed(".multiple-text", {
+    strings:["Backend Developer", "Blogger", "YouTuber", "Frontend Developer", "Football Player"],
+    typeSpeed: 95,
+    backSpeed: 100,
+    backDelay: 500,
+})
